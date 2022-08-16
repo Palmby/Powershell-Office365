fee57c790c793097fb628a7a10a04573c57a7afc
Palmby <kaleb_palmby@yahoo.com>
2022-08-16 18:46:57 -0400

    Get-Mailbox Size Report
    Pulls Mailbox Report for size of Mailboxes in an O365 tenant

---
+++ Mailbox_Size.ps1
@@ -0,0 +1,6 @@
+﻿###############Grabs all users in a tenant and gets their mailbox size#####################
+
+Connect-ExchangeOnline
+
+get-mailbox | get-mailboxstatistics | ft displayname, totalitemsize | out-file C:\temp\mailboxsize.txt
+
