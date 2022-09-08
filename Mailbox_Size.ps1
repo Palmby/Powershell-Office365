978cacad88e953b72ee9c427444978a68c3c703a
Palmby <kaleb_palmby@yahoo.com>
2022-09-08 15:37:09 -0400

    Update Mailbox_Size.ps1

--- Mailbox_Size.ps1
+++ Mailbox_Size.ps1
@@ -3,4 +3,4 @@
 Connect-ExchangeOnline
 
 get-mailbox | get-mailboxstatistics | ft displayname, totalitemsize | out-file C:\temp\mailboxsize.txt
-
+#get-mailbox | get-mailboxstatistics | sort-object TotalItemSize -ascending | Select DisplayName, TotalitemSize
