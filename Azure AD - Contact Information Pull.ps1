ad18c661d65714a72b6f7a97c1473fa3fcbd5303
Palmby <kaleb_palmby@yahoo.com>
2022-09-09 16:18:31 -0400

    Azure AD - Pulls Contact Information
    Saves contact info in the C:\temp folder

---
+++ Azure AD - Contact Information Pull.ps1
@@ -0,0 +1,25 @@
+Connect-AzureAD
+$users = get-azureaduser -all $true 
+
+foreach ($user in $users){
+
+$prop = @{
+
+"Name" = $user.DisplayName
+"Mail" = $user.mail
+"Title" = $user.jobtitle 
+"Department" = $user.department
+"Office Number" = $user.telephoneNumber
+"Mobile" = $user.mobile
+"Office" = $user.PhysicalDeliveryOfficeName
+}
+
+$object = New-Object -TypeName PSObject -propert $prop
+
+foreach ($o in $object)
+{
+    write-output $o | ConvertTo-Json | out-file C:\temp\$o.DisplayName.json 
+}    
+
+}
+
