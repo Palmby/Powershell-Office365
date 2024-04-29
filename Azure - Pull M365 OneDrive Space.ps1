f524c05240834452f21486ae5ef843743f233441
cranky_sysadmin <kaleb_palmby@yahoo.com>
2024-04-29 15:12:45 -0400

    Added OneDrive Script

---
+++ Azure - Pull M365 OneDrive Space.ps1
@@ -0,0 +1,58 @@
+﻿#pull all onedrive users and the size of their OneDrives
+$adminURL = #example: "https://palmby-admin.sharepoint.com/"
+$tenant = #Example: "Palmby"
+
+
+Connect-MgGraph
+
+$users = Get-MgUser -all 
+
+
+
+connect-sposervice -url $adminURL
+
+
+foreach ($user in $users)
+
+    {
+    $e.clear
+
+    $TISDisplayname = $user.DisplayName
+    $TISnickname = $user.userprincipalname.replace('@','_').replace('.','_')
+
+
+    $URL = "https://$($tenant)-my.sharepoint.com/personal/$TISnickname"
+    
+    try{
+    $OData = get-sposite -identity $URL | select Owner, StorageUsageCurrent, Status
+       
+   
+  
+    
+
+   
+    
+    $object = @{
+                    'User' = $TISDisplayName
+                    'CurrentUsage' = ($OData.StorageUsageCurrent /1024) -as [decimal]
+                    'SiteURL' = $URL
+               }
+
+
+     $result = New-Object PSObject -property $object
+     $result | select User,CurrentUsage, SiteURL | sort User | export-csv C:\temp\OneDriveReport.csv -NoTypeInformation -append -force
+     
+
+
+    }
+
+    catch{ write-output "$TISDisplayName error"}
+    }
+    
+
+    
+
+
+
+
+
