a482b5573f36e8e8432a5072f9a5a7d449b1a187
cranky_sysadmin <kaleb_palmby@yahoo.com>
2025-02-21 09:25:03 -0500

    Update Azure - Automate MS Graph Import for automation accounts.ps1

--- Azure - Automate MS Graph Import for automation accounts.ps1
+++ Azure - Automate MS Graph Import for automation accounts.ps1
@@ -1,8 +1,11 @@
 Connect-AzAccount
 
 #Variables
-$resourceGroup = #"REsource group name"
-$AutomationAccount = #"Automation account name"
+$resourceGroup = Read-Host "Resource Group?: "
+$AutomationAccount = Read-Host "Automation Account?: "
+
+##Install Runtime 5.1##
+
 #Install Microsoft.Graph
 $GraphModule = find-module Microsoft.Graph
 $DependencyList = $GraphModule | select -ExpandProperty Dependencies | ConvertTo-Json | ConvertFrom-Json
@@ -14,7 +17,7 @@ foreach ($Dependant in $DependencyList)
     $ModuleName = $Dependant.Name
     $ModuleVersion = $Dependant.Version
     $ContentLink = "https://www.powershellgallery.com/api/v2/package/$ModuleName/$ModuleVersion"
-    New-AzAutomationModule -ResourceGroupName $ResourceGroup -AutomationAccountName $AutomationAccount -Name $ModuleName -ContentLinkUri $ContentLink -ErrorAction Stop | Out-Null
+    New-AzAutomationModule -ResourceGroupName $ResourceGroup -AutomationAccountName $AutomationAccount -Name $ModuleName -ContentLinkUri $ContentLink -runtimeversion 5.1 -ErrorAction Stop | Out-Null
     write-output "$ModuleName being installed to $AutomationAccount"
 }
 
@@ -29,6 +32,68 @@ foreach ($Dependant in $DependencyList)
     $ModuleName = $Dependant.Name
     $ModuleVersion = $Dependant.Version
     $ContentLink = "https://www.powershellgallery.com/api/v2/package/$ModuleName/$ModuleVersion"
-    New-AzAutomationModule -ResourceGroupName $ResourceGroup -AutomationAccountName $AutomationAccount -Name $ModuleName -ContentLinkUri $ContentLink -ErrorAction Stop | Out-Null
+    New-AzAutomationModule -ResourceGroupName $ResourceGroup -AutomationAccountName $AutomationAccount -Name $ModuleName -ContentLinkUri $ContentLink -runtimeversion 5.1 -ErrorAction Stop | Out-Null
+    write-output "$ModuleName being installed to $AutomationAccount"
+}
+
+#install Exchange Online Management 
+$GraphModule = find-module ExchangeOnlineManagement
+$DependencyList = $GraphModule | select -ExpandProperty Dependencies | ConvertTo-Json | ConvertFrom-Json
+$ModuleVersion = $GraphModule.Version
+
+foreach ($Dependant in $DependencyList)
+{
+    start-sleep 20
+    $ModuleName = $Dependant.Name
+    $ModuleVersion = $Dependant.Version
+    $ContentLink = "https://www.powershellgallery.com/api/v2/package/$ModuleName/$ModuleVersion"
+    New-AzAutomationModule -ResourceGroupName $ResourceGroup -AutomationAccountName $AutomationAccount -Name $ModuleName -ContentLinkUri $ContentLink -runtimeversion 5.1 -ErrorAction Stop | Out-Null
+    write-output "$ModuleName being installed to $AutomationAccount"
+}
+
+##install runtime version 7.2##
+
+#Install Microsoft.Graph
+$GraphModule = find-module Microsoft.Graph
+$DependencyList = $GraphModule | select -ExpandProperty Dependencies | ConvertTo-Json | ConvertFrom-Json
+$ModuleVersion = $GraphModule.Version
+
+foreach ($Dependant in $DependencyList)
+{
+    start-sleep 20
+    $ModuleName = $Dependant.Name
+    $ModuleVersion = $Dependant.Version
+    $ContentLink = "https://www.powershellgallery.com/api/v2/package/$ModuleName/$ModuleVersion"
+    New-AzAutomationModule -ResourceGroupName $ResourceGroup -AutomationAccountName $AutomationAccount -Name $ModuleName -ContentLinkUri $ContentLink -runtimeversion 7.2 -ErrorAction Stop | Out-Null
+    write-output "$ModuleName being installed to $AutomationAccount"
+}
+
+#Install Microsoft.Graph.Beta
+$GraphModule = find-module Microsoft.Graph.Beta
+$DependencyList = $GraphModule | select -ExpandProperty Dependencies | ConvertTo-Json | ConvertFrom-Json
+$ModuleVersion = $GraphModule.Version
+
+foreach ($Dependant in $DependencyList)
+{
+    start-sleep 20
+    $ModuleName = $Dependant.Name
+    $ModuleVersion = $Dependant.Version
+    $ContentLink = "https://www.powershellgallery.com/api/v2/package/$ModuleName/$ModuleVersion"
+    New-AzAutomationModule -ResourceGroupName $ResourceGroup -AutomationAccountName $AutomationAccount -Name $ModuleName -ContentLinkUri $ContentLink -runtimeversion 7.2 -ErrorAction Stop | Out-Null
+    write-output "$ModuleName being installed to $AutomationAccount"
+}
+
+#install Exchange Online Management 
+$GraphModule = find-module ExchangeOnlineManagement
+$DependencyList = $GraphModule | select -ExpandProperty Dependencies | ConvertTo-Json | ConvertFrom-Json
+$ModuleVersion = $GraphModule.Version
+
+foreach ($Dependant in $DependencyList)
+{
+    start-sleep 20
+    $ModuleName = $Dependant.Name
+    $ModuleVersion = $Dependant.Version
+    $ContentLink = "https://www.powershellgallery.com/api/v2/package/$ModuleName/$ModuleVersion"
+    New-AzAutomationModule -ResourceGroupName $ResourceGroup -AutomationAccountName $AutomationAccount -Name $ModuleName -ContentLinkUri $ContentLink -runtimeversion 7.2 -ErrorAction Stop | Out-Null
     write-output "$ModuleName being installed to $AutomationAccount"
 }
